837475cf147c5d309a8f907d956f379b33f66533
Piotr Pakulski <ppakulski@box.com>
2022-05-08 14:33:10 +0200

    Adding first logger skeleton

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <fstream>
+//#include "logger.h"
+#include <sstream>
+
+void    openLog(std::string fileName);
+void    saveLog(std::string msg);
+void    closeLog(void);
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
